ee90c7202ae287ac6bf1df6ccab8f6aeeca4165e
UNIDRA <57227914+UNIDRA@users.noreply.github.com>
2020-04-16 22:05:18 -0700

    Added Portfolio Files from Repl.it

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links Anmol Nayak Period:7</p>'; ?> 
+    <ul>
+      <li><a href="index.html">Home</a></li></br>
+      <li><a href="Nayak_AboutMe.html">About Me</a></li></br>
+      <li><a href="Nayak_Projects.html">Projects</a></li></br>
+      <li><a href="Nayak_Resume.html">Resume</a></li></br>
+      <li><a href="Nayak_ContactMe.html">Contact Me</a></li></br>
+    <ul>
+  </body>
+</html>
